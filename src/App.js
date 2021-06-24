2bd81bbd2ea38f544bbb0adb503fbf7481bd77b7
shubhpatil <patilpshubh@gmail.com>
2021-06-24 23:12:29 +0530

    redux

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { useSelector, useDispatch } from "react-redux";
+
+function App() {
+  const counter = useSelector((state) => state.counter);
+  const Dispatch = useDispatch();
+
+  return (
+    <div>
+      <h1>React Redux</h1>
+      <h1>{counter}</h1>
+      <button onClick={() => Dispatch({ type: "INCREMENT" })}>+</button>
+      <button onClick={() => Dispatch({ type: "DECREMENT" })}>-</button>
+    </div>
+  );
+}
+
+export default App;
